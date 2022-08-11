e1b2ffb0510f507106b828d770f523daf77e862b
Edgamer63 <edgamer63@gmail.com>
2022-08-10 22:59:10 -0400

    Añadiendo lista de notas

--- src/main/java/cl/infoclub/fsj/GIT/modelo/Alumno.java
+++ src/main/java/cl/infoclub/fsj/GIT/modelo/Alumno.java
@@ -1,7 +1,41 @@
 package cl.infoclub.fsj.GIT.modelo;
 
-public class Alumno {
+import java.util.ArrayList;
 
-	private double nota;
+public class Alumno extends Persona {
+
+	private ArrayList<Double> listaNotas = new ArrayList<>();
+	
+	private int numeroLista;
+	private String curso;
+	
+	
+	
+	public Alumno(double nota, int numeroLista, String curso) {
+		super();
+		this.numeroLista = numeroLista;
+		this.curso = curso;
+	}
+	public Alumno() {
+		super();
+	}
+	public int getNumeroLista() {
+		return numeroLista;
+	}
+	public void setNumeroLista(int numeroLista) {
+		this.numeroLista = numeroLista;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	@Override
+	public String toString() {
+		return "Alumno [numeroLista=" + numeroLista + ", curso=" + curso + "]";
+	}
+	
+	
 	
 }
