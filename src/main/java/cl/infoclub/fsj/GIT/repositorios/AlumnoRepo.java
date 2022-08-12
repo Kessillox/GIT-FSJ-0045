a7b61fc9cf5d60b5f092d679c5cf10bb0aade507
Lisbeth Calcina <jliscayo@gmail.com>
2022-08-11 23:11:05 -0400

    actualizacion metodos

--- src/main/java/cl/infoclub/fsj/GIT/repositorios/AlumnoRepo.java
+++ src/main/java/cl/infoclub/fsj/GIT/repositorios/AlumnoRepo.java
@@ -7,34 +7,6 @@ import cl.infoclub.fsj.GIT.modelo.Alumno;
 
 public abstract class AlumnoRepo implements IAlumno {
 
-ArrayList<Alumno> Alumno = new ArrayList<Alumno>();
-	
-	
-	public ArrayList<Alumno> getAlumno() {
-		return Alumno;
-	}
-	
-	public void setAlumno(ArrayList<Alumno> alumno) {
-		this.Alumno = alumno;
-	}
 
-	public void crear (Alumno al) {
-		this.Alumno.add(al);
-		
-	}
-	
-	public void listar() {
-		for (int i= 0;i<Alumno.size();i++) {
-				System.out.println(getAlumno().get(i));
-			
-		}
-	}
-	
-	public void actualizar (Alumno alumno) {
-
-		
-	}
-	public void eliminar(Alumno alumno) {
-	}
 
 }
--- src/main/java/cl/infoclub/fsj/GIT/repositorios/ProfesorRepo.java
+++ src/main/java/cl/infoclub/fsj/GIT/repositorios/ProfesorRepo.java
@@ -8,35 +8,6 @@ import cl.infoclub.fsj.GIT.modelo.Profesor;
 public abstract class ProfesorRepo implements IProfesor {
 	
 	
-	ArrayList<Profesor> Profesor = new ArrayList<Profesor>();
-	
-	
-	public ArrayList<Profesor> getProfesor() {
-		return Profesor;
-	}
 
-	public void setProfesor(ArrayList<Profesor> profesor) {
-		Profesor = profesor;
-	}
-
-
-	public void crear (Profesor pr) {
-		this.Profesor.add(pr);
-		
-	}
-	
-	public void listar() {
-		for (int i= 0;i<Profesor.size();i++) {
-				System.out.println(getProfesor().get(i));
-			
-		}
-	}
-	
-	public void actualizar (Profesor profesor) {
-
-		
-	}
-	public void eliminar(Profesor profesor) {
-	}
 
 }
